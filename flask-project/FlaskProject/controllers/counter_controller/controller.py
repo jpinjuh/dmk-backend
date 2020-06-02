8d85c409dba8c6022ee98dbcbec254cbc3adca5d
andjelabosnjak <andjela.bosnjak30@gmail.com>
2020-06-02 09:53:42 +0200

    generate in CounterController

--- flask-project/FlaskProject/controllers/counter_controller/controller.py
+++ flask-project/FlaskProject/controllers/counter_controller/controller.py
@@ -50,6 +50,62 @@ class CounterController(BaseController):
         raise NotImplementedError("To be implemented")
 
     @staticmethod
+    def generate(counter_id):
+        """
+        Use this method to generate new counter value
+        :param counter_id: counter identifier unique
+        :type counter_id: uuid
+        :return: counter value or None
+        :rtype: String
+        """
+        # Broj pokusaja dohvata brojaca
+        max_repeat = 5
+        counter_obj = CounterController.get_one(counter_id)
+
+        if counter_obj.counter is None:
+            return None
+        # Ako nije definiran brojac
+        if counter_obj.counter.value is None:
+            # Ako je barem definiran start_from
+            if counter_obj.counter.start_from is not None:
+                counter_obj.counter.value = str(counter_obj.counter.start_from)
+                try:
+                    counter_obj.counter.add()
+                    counter_obj.counter.commit_or_rollback()
+                except Exception as e:
+                    raise FlaskProjectLogException(Status(-101, str(e)).__dict__)
+                # Ako postoji neki izraz
+                if counter_obj.counter.expression is not None:
+                    counter_obj.check_restart()
+                    return counter_obj.replace_expression()
+                # Ako ne postoji izraz
+                else:
+                    counter_obj.check_restart()
+                    return counter_obj.counter.value
+            # Ako nisu definirani brojac i start_from
+            else:
+                return None
+        # Ako je definiran brojac
+        else:
+            for i in range(max_repeat):
+                counter_obj.counter.value = int(counter_obj.counter.value) + 1
+                try:
+                    counter_obj.counter.add()
+                    counter_obj.counter.commit_or_rollback()
+                    break
+                except Exception as e:
+                    if i != (max_repeat - 1):
+                        counter_obj.counter.value = int(counter_obj.counter.value) - 1
+                        continue
+                    raise FlaskProjectLogException(Status(-101, str(e)).__dict__)
+            if counter_obj.counter.expression is not None:
+                counter_obj.check_restart()
+                return counter_obj.replace_expression()
+            else:
+                counter_obj.check_restart()
+                return counter_obj.counter.value
+
+    @staticmethod
     def list_autocomplete(search):
         raise NotImplementedError("To be implemented")
 
